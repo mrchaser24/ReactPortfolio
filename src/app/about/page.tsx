b6fce7818af5e908f8eb604b48ea2f49c68fc733
Chase Arcos HERMOSISIMA <chase.hermosisima@ext.essilor.com>
2024-08-10 01:43:44 +0800

    TEST

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -25,6 +25,7 @@ export default function About() {
             <div>
                 <p className='about-description'>Hi, I&lsquom Chase Arcos Hermosisima. I&lsquom a software engineer with a passion for programming and technology. I am a person who loves solving problems and creating solutions to a business. My goal is to continue learning and improving my skills in various programming languages and frameworks.</p>
             </div>
+            test
 
             <Summary/>
         </main>
